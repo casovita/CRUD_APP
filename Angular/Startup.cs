e8e453c7cdc49d8457a91dbed025ab50c544cab9
roman <casovita@gmail.com>
2019-08-23 23:07:44 +0300

    updated jquery version

--- Angular/Startup.cs
+++ Angular/Startup.cs
@@ -1,3 +1,4 @@
+using IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -21,6 +22,7 @@ namespace Angular
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            DependencyContainer.RegisterServices(services);
 
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
---
+++ IoC/DependencyContainer.cs
@@ -0,0 +1,16 @@
+﻿using Core;
+using Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace IoC
+{
+    public static class DependencyContainer
+    {
+
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddSingleton<IService, Service>();
+        }
+    }
+}
